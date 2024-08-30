c79a49125bb491efa2c135ddf1858d59ffe33a59
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2024-08-30 10:39:29 +1000

    chore: add app config, env file

---
+++ src/config/app/config.module.ts
@@ -0,0 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+
+import configuration from './configuration';
+import { ConfigAppService } from './config.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
+  providers: [ConfigAppService],
+})
+export class AppConfigModule {}
